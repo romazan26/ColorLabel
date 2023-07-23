8c91108d22c3cd34b8dc994d58a1e6a35506f09d
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-07-23 19:35:13 +0300

    FirstGoogResult

--- ColorLabel/ContentView.swift
+++ ColorLabel/ContentView.swift
@@ -8,13 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var value = 0.0
+    @State var valueTF = ""
+    @State var valueTwo = 0.0
+    @State var valueThree = 0.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Capsule()
+                .frame(width: 300, height: 100)
+//                .foregroundColor(Color(uiColor:
+//                                        UIColor(red: CGFloat(value),
+//                                                green: CGFloat(valueTwo),
+//                                                blue: CGFloat(valueThree),
+//                                                alpha: 1)))
+                .foregroundColor(Color(cgColor: CGColor(red: CGFloat(value / 255),
+                                                        green: CGFloat(valueTwo / 255),
+                                                        blue: CGFloat(valueThree / 255),
+                                                        alpha: 1)))
+            SliderColorMore(value: $value, textFild: $valueTF)
+            SliderColorMore(value: $valueTwo, textFild: $valueTF)
+            SliderColorMore(value: $valueThree, textFild: $valueTF)
+            Spacer()
         }
+        
         .padding()
     }
 }
@@ -22,5 +40,20 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+struct SliderColorMore: View {
+    @Binding var value: Double
+    @Binding var textFild: String
+    
+    var body: some View {
+        HStack {
+            
+            Text("\(lround(value))")
+                .frame(height: 20)
+            Slider(value: $value, in: 0...255, step: 1)
+            TextField("255", text: $textFild)
+            }
+        
     }
 }
