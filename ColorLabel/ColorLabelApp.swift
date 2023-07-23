aff2254e1892fe72eac84a2a87c89bd448200ac0
romazan26 <130604263+romazan26@users.noreply.github.com>
2023-07-23 16:36:29 +0300

    Initial Commit

---
+++ ColorLabel/ColorLabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorLabelApp.swift
+//  ColorLabel
+//
+//  Created by Роман on 23.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorLabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
